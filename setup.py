5d48d6f2b39964aef792f5d0c93d7e90afda4bf4
RoboticsYY <yu.yan@intel.com>
2019-11-13 15:21:04 +0800

    Explicitly install package.xml file

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+from glob import glob
 from setuptools import setup
 
 package_name = 'urdfdom_py'
@@ -8,8 +9,10 @@ setup(
     package_dir={'': 'src'},
     packages=['urdf_parser_py', 'urdf_parser_py.xml_reflection'],
     data_files=[
-        ('scripts/display_urdf',
-        ['package.xml']),
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/scripts', glob('scripts/display_urdf')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
